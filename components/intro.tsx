7de67dc1abe102cb03f94059a84e99bbe1fe969c
snorng1 <89033297+snorng1@users.noreply.github.com>
2024-11-20 10:37:44 +0000

    First commit

---
+++ components/intro.tsx
@@ -0,0 +1,28 @@
+import Image from 'next/image'
+import authorImage from '@/public/images/authors/steph.jpg'
+
+export default function Intro() {
+  return (
+    <section className='flex flex-col-reverse items-start gap-x-10 gap-y-4 pb-24 md:flex-row md:items-center'>
+      <div className='mt-2 flex-1 md:mt-0'>
+        <h1 className='title no-underline'>Hey, I&#39;m Stéphane.</h1>
+        <p className='mt-3 font-light text-muted-foreground'>
+          I&#39;m a web developer based in Montreal, Canada. I&#39;m
+          passionate about learning new technologies and sharing knowledge with
+          others.
+        </p>
+      </div>
+      <div className='relative'>
+        <Image
+        //   className='flex-1 rounded-lg grayscale'
+          className='flex-1 rounded-lg'
+          src={authorImage}
+          alt='Stéphane Norng'
+          width={250}
+          height={250}
+          priority
+        />
+      </div>
+    </section>
+  )
+}
--- next.config.ts
+++ next.config.ts
@@ -1,7 +1,26 @@
-import type { NextConfig } from "next";
+import remarkGfm from 'remark-gfm'
+import createMDX from '@next/mdx'
+ 
+/** @type {import('next').NextConfig} */
+const nextConfig = {
+  // Allow .mdx extensions for files
+  pageExtensions: ['js', 'jsx', 'md', 'mdx', 'ts', 'tsx'],
+  // Optionally, add any other Next.js config below
+}
+ 
+const withMDX = createMDX({
+  // Add markdown plugins here, as desired
+  options: {
+    remarkPlugins: [remarkGfm],
+    rehypePlugins: [],
+  },
+})
+ 
+// Combine MDX and Next.js config
+export default withMDX(nextConfig)
 
-const nextConfig: NextConfig = {
-  /* config options here */
-};
+// function throwError(envVar) {
+//   throw `Abort: You need to define ${envVar} in the .env file.`
+// }
 
-export default nextConfig;
+// if (!process.env.RESEND_API_KEY) return throwError('RESEND_API_KEY');
